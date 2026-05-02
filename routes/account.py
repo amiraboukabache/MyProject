61834d868e63f28e7b6f007d1e905a34cffba671
Amira Boukabache <amiraboukabache@gmail.com>
2026-05-02 14:55:30 +0100

    fix: remove venv and zip from git

--- routes/account.py
+++ routes/account.py
@@ -151,7 +151,7 @@ def update_account():
 @account_bp.route("/account", methods=["DELETE"])
 def delete_account():
     token = get_token_from_request()
-    if not token:
+    if not token:git rm -r --cached venv/
 
         return jsonify({"error": "Token manquant"}), 401
 
